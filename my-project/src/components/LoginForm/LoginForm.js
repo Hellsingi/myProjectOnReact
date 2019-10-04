9d79d4a81ca5407ee45cf97003a5fd782b103ea7
Hellsingi <f.c.realmadrid@mail.ru>
2019-10-04 15:28:08 +0300

    rEGROuping project structure

--- my-project/src/components/LoginForm/LoginForm.js
+++ my-project/src/components/LoginForm/LoginForm.js
@@ -1,47 +1,45 @@
 import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
-import myInput from "../Field/myInput";
-import { requiredInput } from "../../Validation/index";
-import "./LoginForm.scss";
+import myInput from "../common/Field/myInput";
+import { requiredInput } from "../../Validation/validation";
+import "./loginForm.scss";
 
 class LoginForm extends Component {
-//   submit = values => {
-//     console.log(JSON.stringify(values));
-//     alert(JSON.stringify(values));
-//   };
+  //   submit = values => {
+  //     console.log(JSON.stringify(values));
+  //     alert(JSON.stringify(values));
+  //   };
 
   render() {
     const { handleSubmit, reset } = this.props;
 
     return (
       <>
-        <div className="image">
-          <form onSubmit={handleSubmit} className="formLogin">
-            <Field
-              name="username"
-              component={myInput}
-              type="text"
-              placeholder="Username"
-              validate={requiredInput}
-            />
-            <br />
-            <Field
-              name="password"
-              component={myInput}
-              type="password"
-              placeholder="Password"
-              validate={requiredInput}
-            />
-            <div>
-              <button type="button" onClick={reset}>
-                Clean form
-              </button>
-              <button type="submit" onClick={this.submit}>
-                Submit
-              </button>
-            </div>
-          </form>
-        </div>
+        <form onSubmit={handleSubmit} className="formLogin">
+          <Field
+            name="username"
+            component={myInput}
+            type="text"
+            placeholder="Username"
+            validate={requiredInput}
+          />
+          <br />
+          <Field
+            name="password"
+            component={myInput}
+            type="password"
+            placeholder="Password"
+            validate={requiredInput}
+          />
+          <div>
+            <button type="button" onClick={reset}>
+              Clean form
+            </button>
+            <button type="submit" onClick={this.submit}>
+              Submit
+            </button>
+          </div>
+        </form>
       </>
     );
   }
@@ -53,8 +51,8 @@ class LoginForm extends Component {
 // };
 
 LoginForm = reduxForm({
-  form: "login",
-//   onSubmit
+  form: "login"
+  //   onSubmit
 })(LoginForm);
 
 export default LoginForm;
